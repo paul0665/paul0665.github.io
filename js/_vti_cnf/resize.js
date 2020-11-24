d147232c9c7a978937f0fdb5ef9316c7623bfe34
Paul <47947919+paul0665@users.noreply.github.com>
2020-11-24 12:47:23 +0800

    Add files via upload

--- js/_vti_cnf/resize.js
+++ js/_vti_cnf/resize.js
@@ -4,7 +4,7 @@ vti_modifiedby:SR|LAPTOP-9OLTOJMP\\admin
 vti_timelastmodified:TR|24 Nov 2020 02:49:20 -0000
 vti_timecreated:TR|21 Nov 2020 06:51:51 -0000
 vti_extenderversion:SR|12.0.0.0
-vti_backlinkinfo:VX|contact.html docs/default.html docs/contact.html projects.html docs/projects.html tarshcan/test1.html default.html head.html tarshcan/projects.html docs/head.html
+vti_backlinkinfo:VX|contact.html docs/index.html docs/contact.html projects.html docs/projects.html tarshcan/test1.html docs/head.html head.html tarshcan/projects.html index.html
 vti_nexttolasttimemodified:TW|21 Nov 2020 06:58:31 -0000
 vti_cacheddtm:TX|24 Nov 2020 02:49:20 -0000
 vti_filesize:IR|621
